41b6d0bec08dbabf7e16517a2b776e6ea8edfba2
AristideExe <aristidepr@gmail.com>
2024-01-12 15:28:42 +0100

    Ajoute le routeur React

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Layout from './pages/Layout/Layout';
+import Links from './pages/Links/Links';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={"/links"} element={<Links />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
